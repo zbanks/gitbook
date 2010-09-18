349a38fabcf8cf81cfa53c3385a90cc99480191d
zbanks <zach@zbanks.net>
2010-09-17 23:55:28 -0400

    Python backend; HTML/JS start

---
+++ app/gitbook.js~
@@ -0,0 +1,63 @@
+var MANIFEST_FILE = "/manifest.data.json"; // One file that cannot change
+var DEBUG = function(x){console.log(x);};
+//var DEBUG = $.noop;
+var manifest = {};
+var info = {};
+var wall = {};
+
+$.ajaxSetup({ dataType: "json", processData: false });
+
+function load_manifest(){
+    DEBUG("loading manifest");
+    $.get(MANIFEST_FILE, null, function(data){
+        manifest = data;
+        DEBUG("manifest loaded");
+        DEBUG(manifest);
+        load_info();
+    });
+}
+
+function load_friends(){
+    //TODO
+    load_wall();
+}
+
+function load_wall(){
+    DEBUG("loading wall");
+    $.get(manifest.data.wall, null, function(data){
+        wall = data;
+        DEBUG("wall loaded");
+        DEBUG(wall);
+        update_wall();
+    });
+}
+
+function load_info(){
+    DEBUG("loading info");
+    $.get(manifest.data.info, null, function(data){
+        info = data;
+        DEBUG("info loaded");
+        DEBUG(info);
+        //update_info();
+        load_friends();
+    });
+}
+
+function update_wall(){
+    var walldiv = $("div#wall");
+    walldiv.html(""); // clear
+    for(var i = 0; i < wall.posts.length; i++){
+        $("#wall_post").render(wall.posts[i]).appendTo(walldiv);
+    }
+}
+
+$(document).ready(function(){
+    /* Test -- copy manifest.data.json to wall.data.json
+    $.get("http://localhost:8000/manifest.data.json", "", function(data){
+        console.log(data);
+        $.post("http://localhost:8000/wall.data.json", data, console.log);
+    }, "text");
+    */
+    load_manifest();
+});
+
---
+++ app/gitbook.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# http://blog.modp.com/2008/11/minimal-http-file-server-in-python.html
+# v1.0 23-Nov-2008 Nick Galbreath
+# PUBLIC DOMAIN, but it would be nice if you gave me credit ;-)                      
+import os, os.path, mimetypes, wsgiref.simple_server
+import json
+
+port = 8000  # anything
+root = os.path.expanduser('/tmp/gitbook/') #TODO
+
+def makelink(dir, file):
+    link = (os.path.join(dir,file))[len(root):]
+    return '<a href="%s">%s</a><br>\n' % (link, file)
+def run(environ, start_response):
+    cmd = environ['PATH_INFO']
+    filename = os.path.abspath(os.path.join(root,cmd[cmd.find('/')+1:]))
+    data = ''
+    mtype = 'text/plain'
+    if not filename.startswith(root):
+        status = '403 Forbidden'
+        data = 'Request outside of root directory\n'
+    else:
+        status = '200 OK'
+        try:
+            if os.path.isdir(filename):
+                mtype = 'text/html'
+                files = sorted(os.listdir(filename))
+                links = [makelink(filename,f) for f in files]
+                data = '<html><head><title>' + filename + \
+                    '</title></head><body>\n' + makelink(filename, '..')+ \
+                    ''.join(links) + '</body></html>\n'
+            else:
+                print environ['REQUEST_METHOD']
+                if environ['REQUEST_METHOD'] == 'POST':
+                    datasize = int(environ['CONTENT_LENGTH'])
+                    data = environ['wsgi.input'].read(datasize)
+                    print '\n', datasize, data
+                    f = open(filename, 'wb'); f.write(data); f.close()
+                    m = mimetypes.guess_type(filename)
+                    if m[0] is not None: mtype = m[0]
+                else:
+                    f = open(filename); data = f.read(); f.close()
+                    m = mimetypes.guess_type(filename)
+                    print m
+                    if m[0] is not None: mtype = m[0]
+        except IOError,e:
+            data = str(e) + '\n'
+            status = "404 Not Found"
+    start_response(status, [('Content-Type', mtype)])
+    return [ data ]
+if __name__ == '__main__':
+    httpd = wsgiref.simple_server.make_server('', port, run)
+    sa = httpd.socket.getsockname()
+    httpd.serve_forever()
